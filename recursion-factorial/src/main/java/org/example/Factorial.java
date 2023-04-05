d0a5f90947abd875b741d7d3e88bb202a464b042
Ragini Patel <ragini.patel@tntra.io>
2023-04-05 11:14:18 +0530

    factorial added

--- recursion-factorial/src/main/java/org/example/Factorial.java
+++ recursion-factorial/src/main/java/org/example/Factorial.java
@@ -1,4 +1,13 @@
 package org.example;
 
 public class Factorial {
+    public int Factorial(int k){
+        if(k>0){
+            return k * Factorial(k-1);
+        }
+        else {
+            return 1;
+        }
+    }
+
 }
--- recursion-factorial/src/main/java/org/example/Main.java
+++ recursion-factorial/src/main/java/org/example/Main.java
@@ -3,5 +3,8 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Factorial f = new Factorial();
+        System.out.println(f.Factorial(5));
     }
+
 }
